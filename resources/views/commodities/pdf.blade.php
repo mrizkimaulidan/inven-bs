2a34336e64fed8f41a55e6283c94996d776cffab
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-07-26 13:38:37 +0800

    remove unused line

--- resources/views/commodities/pdf.blade.php
+++ resources/views/commodities/pdf.blade.php
@@ -16,38 +16,38 @@
     margin-left: auto;
     margin-right: auto;
   }
+
   .page-break {
     page-break-after: always;
   }
 </style>
 
 <body>
-    @foreach($commodities as $key => $commodity)
-    <table class="center" border="1" cellpadding="10" cellspacing="0">
-      <tr>
-        <td colspan="2">Barang Milik {{$sekolah}}</td>
-      </tr>
-      <tr>
-        <th>Kode Barang : </th>
-        <td>{{ $commodity->item_code }}</td>
-      </tr>
-      <tr>
-        <th>Nama Barang : </th>
-        <td>{{ $commodity->name }}</td>
-      </tr>
-      <tr>
-        <th>Asal Perolehan : </th>
-        <td>{{ $commodity->school_operational_assistance->name }}</td>
-      </tr>
-    </table>
-    <br>
-    @if ($key!=0)
-      @if (($key+1) % 4==0)
-        <div class="page-break"></div>
-        {{-- {{dd($key)}} --}}
-      @endif
-    @endif
-    @endforeach
+  @foreach($commodities as $key => $commodity)
+  <table class="center" border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <td colspan="2">Barang Milik {{$sekolah}}</td>
+    </tr>
+    <tr>
+      <th>Kode Barang : </th>
+      <td>{{ $commodity->item_code }}</td>
+    </tr>
+    <tr>
+      <th>Nama Barang : </th>
+      <td>{{ $commodity->name }}</td>
+    </tr>
+    <tr>
+      <th>Asal Perolehan : </th>
+      <td>{{ $commodity->school_operational_assistance->name }}</td>
+    </tr>
+  </table>
+  <br>
+  @if ($key!=0)
+  @if (($key+1) % 4==0)
+  <div class="page-break"></div>
+  @endif
+  @endif
+  @endforeach
 </body>
 
 </html>
