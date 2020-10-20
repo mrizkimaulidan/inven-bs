f47e0fec82e7efb23e476669028631839d6e4dec
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-20 09:32:01 +0800

    sidebar titles

--- resources/views/layouts/stisla/sidebar.blade.php
+++ resources/views/layouts/stisla/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="index.html">{{ config('app.name') }}</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">{{ substr(config('app.name'), 0, 2) }}</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
