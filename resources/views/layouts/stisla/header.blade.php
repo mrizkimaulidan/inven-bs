b129902af4cc23291e06487c7c14c285f44a40c0
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-20 09:50:33 +0800

    dynamic title

--- resources/views/layouts/stisla/header.blade.php
+++ resources/views/layouts/stisla/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>{{ $title }} &mdash; Stisla</title>
+    <title>{{ $title }} &mdash; {{ config('app.name') }}</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ url('assets/bootstrap/css/bootstrap.min.css') }}">
