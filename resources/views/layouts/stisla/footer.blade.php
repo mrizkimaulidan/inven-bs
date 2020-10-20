052f7c8c00d0b0be902fd0ed7b9f31eff4fd8c3b
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-20 09:53:17 +0800

    dynamic years

--- resources/views/layouts/stisla/footer.blade.php
+++ resources/views/layouts/stisla/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        Copyright &copy; {{ date('Y') }}
+        <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
     </div>
     <div class="footer-right">
         2.3.0
