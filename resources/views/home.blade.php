06dc3634930db82245657e84837b855724c6c230
mrizkimaulidan <mrizkimaulidan@hotmail.com>
2020-10-20 09:07:02 +0800

    changing class from bg-danger to bg-success

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
   </div>
   <div class="col-lg-3 col-md-6 col-sm-6 col-12">
     <div class="card card-statistic-1">
-      <div class="card-icon bg-danger">
+      <div class="card-icon bg-success">
         <i class="fas fa-fw fa-check-circle"></i>
       </div>
       <div class="card-wrap">
