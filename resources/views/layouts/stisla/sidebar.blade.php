1f62572bd369f2dc280bdaebbfa9c8978eb7d967
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-01-20 22:32:47 +0800

    chore: change href to dashboard

--- resources/views/layouts/stisla/sidebar.blade.php
+++ resources/views/layouts/stisla/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar">
 	<aside id="sidebar-wrapper">
 		<div class="sidebar-brand">
-			<a href="index.html">{{ config('app.name') }}</a>
+			<a href="{{ route('home') }}">{{ config('app.name') }}</a>
 		</div>
 		<div class="sidebar-brand sidebar-brand-sm">
-			<a href="index.html">{{ substr(config('app.name'), 0, 2) }}</a>
+			<a href="{{ route('home') }}">{{ substr(config('app.name'), 0, 2) }}</a>
 		</div>
 		<ul class="sidebar-menu">
 			<li class="menu-header">Dashboard</li>
