06d3ff657c2d52a71206889d13b5bac5ac7cb4d6
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-02-09 19:28:56 +0800

    feat: :sparkles: custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+	<title>404 Halaman Tidak Ditemukan &mdash; {{ config('app.name') }}</title>
+
+	<!-- General CSS Files -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+		integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<!-- CSS Libraries -->
+
+	<!-- Template CSS -->
+	<link rel="stylesheet" href="../assets/css/style.css">
+	<link rel="stylesheet" href="../assets/css/components.css">
+</head>
+
+<body>
+	<div id="app">
+		<section class="section">
+			<div class="container mt-5">
+				<div class="page-error">
+					<div class="page-inner">
+						<h1>404</h1>
+						<div class="page-description">
+							Halaman yang Anda cari tidak ditemukan.
+						</div>
+						<div class="page-search">
+							<div class="mt-3">
+								<a href="{{ route('home') }}">Kembali ke dashboard</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="simple-footer mt-5">
+					Copyright &copy; {{ config('app.name') }} {{ date('Y') }}
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<!-- General JS Scripts -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+	</script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+	<script src="../assets/js/stisla.js"></script>
+
+	<!-- JS Libraies -->
+
+	<!-- Template JS File -->
+	<script src="../assets/js/scripts.js"></script>
+	<script src="../assets/js/custom.js"></script>
+
+	<!-- Page Specific JS File -->
+</body>
+
+</html>
