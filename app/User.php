97c604431cb2d9b0beecf8d99666c25bdf809479
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-03-08 20:55:15 +0800

    dependency #36: :heavy_plus_sign: added spatie permission package and added starter config

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
