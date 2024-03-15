66e901b68d25f96fece4bbf4e122b42a81f8975f
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-03-15 10:27:18 +0800

    feat #36: :sparkles: added store feature on role

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRoleRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -30,9 +33,28 @@ class RoleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRoleRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        DB::beginTransaction();
+
+        try {
+            $role = Role::create([
+                'name' => $validated['name'],
+                'guard_name' => 'web'
+            ]);
+
+            $permissions = Permission::find($validated->permissions);
+            $role->syncPermissions($permissions);
+            DB::commit();
+
+            return redirect()->route('peran-dan-hak-akses.index')->with('success', 'Data berhasil ditambahkan!');
+        } catch (\Exception $ex) {
+            DB::rollBack();
+
+            return redirect()->route('peran-dan-hak-akses.index')->with('error', 'Data gagal ditambahkan!');
+        }
     }
 
     /**
