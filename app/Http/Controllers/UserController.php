0ee579a602880124c7807ddc9f55bdeb459e4efa
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-03-30 10:35:08 +0800

    refactor: :recycle: ignore logged in user to be listed on table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::all()->except(auth()->id());
         $roles = Role::all();
 
         return view('users.index', compact('users', 'roles'));
