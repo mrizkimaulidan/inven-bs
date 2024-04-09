16f9b17960202b7ef281e5ff5ad4a0250ed9d65a
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2024-04-09 17:43:37 +0800

    feat: :sparkles: show count users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.index', ['title' => 'Halaman Data Pengguna', 'page_heading' => 'Daftar Pengguna'])
 
 @section('content')
+<div class="row justify-content-center">
+	@foreach ($roles as $role)
+	<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+		<div class="card card-statistic-1">
+			<div class="card-icon bg-primary">
+				<i class="fas fa-users"></i>
+			</div>
+			<div class="card-wrap">
+				<div class="card-header">
+					<h4>Total {{ $role->name }}</h4>
+				</div>
+				<div class="card-body">
+					{{ $role->users_count }}
+				</div>
+			</div>
+		</div>
+	</div>
+	@endforeach
+</div>
 <div class="card">
 	<div class="card-body">
 		@include('utilities.alert')
