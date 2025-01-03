fd752a582359f2d21f65af3173b950a371bf9e27
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2025-01-04 03:13:53 +0800

    refactor: :recycle: redesign alert message

--- resources/views/utilities/alert.blade.php
+++ resources/views/utilities/alert.blade.php
@@ -1,15 +1,14 @@
 @if ($errors->store->any())
-<div class="alert alert-light alert-dismissible fade show border border-danger" role="alert">
-	<strong>
-		<i class="fas fa-exclamation"></i>
-		Gagal!
-	</strong>
-
-	<ul>
-		@foreach ($errors->store->all() as $message)
-		<li>{{ $message }}</li>
-		@endforeach
-	</ul>
+<div class="alert alert-danger alert-has-icon alert-dismissible fade show" role="alert">
+	<div class="d-flex align-items-start">
+		<i class="fas fa-exclamation-circle alert-icon mr-3" style="font-size: 1.5rem;"></i>
+		<div>
+			<div class="alert-title font-weight-bold">Gagal</div>
+			@foreach ($errors->store->all() as $message)
+			<p><i class="fas fa-arrow-right mr-2"></i>{{ $message }}</p>
+			@endforeach
+		</div>
+	</div>
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 	</button>
@@ -17,17 +16,16 @@
 @endif
 
 @if ($errors->update->any())
-<div class="alert alert-light alert-dismissible fade show border border-danger" role="alert">
-	<strong>
-		<i class="fas fa-exclamation"></i>
-		Gagal!
-	</strong>
-
-	<ul>
-		@foreach ($errors->update->all() as $message)
-		<li>{{ $message }}</li>
-		@endforeach
-	</ul>
+<div class="alert alert-danger alert-has-icon alert-dismissible fade show" role="alert">
+	<div class="d-flex align-items-start">
+		<i class="fas fa-exclamation-circle alert-icon mr-3" style="font-size: 1.5rem;"></i>
+		<div>
+			<div class="alert-title font-weight-bold">Gagal</div>
+			@foreach ($errors->update->all() as $message)
+			<p><i class="fas fa-arrow-right mr-2"></i>{{ $message }}</p>
+			@endforeach
+		</div>
+	</div>
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 	</button>
@@ -35,11 +33,14 @@
 @endif
 
 @session('success')
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-	<strong>
-		<i class="fas fa-thumbs-up"></i>
-		Berhasil!
-	</strong> {{ session('success') }}
+<div class="alert alert-success alert-has-icon alert-dismissible fade show" role="alert">
+	<div class="d-flex align-items-start">
+		<i class="fas fa-check-circle alert-icon mr-3" style="font-size: 1.5rem;"></i>
+		<div>
+			<div class="alert-title font-weight-bold">Berhasil</div>
+			{{ session('success') }}
+		</div>
+	</div>
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 	</button>
@@ -47,11 +48,14 @@
 @endsession
 
 @session('error')
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-	<strong>
-		<i class="fas fa-thumbs-down"></i>
-		Gagal!
-	</strong> {{ session('error') }}
+<div class="alert alert-danger alert-has-icon alert-dismissible fade show" role="alert">
+	<div class="d-flex align-items-start">
+		<i class="fas fa-exclamation-circle alert-icon mr-3" style="font-size: 1.5rem;"></i>
+		<div>
+			<div class="alert-title font-weight-bold">Gagal</div>
+			{{ session('error') }}
+		</div>
+	</div>
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 	</button>
