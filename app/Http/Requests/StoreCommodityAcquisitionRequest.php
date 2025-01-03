72767c8a3c1ea26b92e9de56543ded1e010e663c
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2025-01-04 03:14:49 +0800

    fix: :bug: added missing error bag on form class request

--- app/Http/Requests/StoreCommodityAcquisitionRequest.php
+++ app/Http/Requests/StoreCommodityAcquisitionRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCommodityAcquisitionRequest extends FormRequest
 {
+    protected $errorBag = 'store';
+
     /**
      * Determine if the user is authorized to make this request.
      */
--- app/Http/Requests/UpdateCommodityAcquisitionRequest.php
+++ app/Http/Requests/UpdateCommodityAcquisitionRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateCommodityAcquisitionRequest extends FormRequest
 {
+    protected $errorBag = 'update';
+
     /**
      * Determine if the user is authorized to make this request.
      */
