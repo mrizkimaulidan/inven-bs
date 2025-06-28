5594b9589cd629c64bbe7c489e78f29324754bdf
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-06-28 16:14:53 +0700

    feat(commodity): add QR code generation endpoint

--- app/Http/Controllers/API/CommodityController.php
+++ app/Http/Controllers/API/CommodityController.php
@@ -6,6 +6,8 @@ use App\Commodity;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ShowCommodityResource;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Crypt;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class CommodityController extends Controller
 {
@@ -20,6 +22,31 @@ class CommodityController extends Controller
             'code' => Response::HTTP_OK,
             'message' => 'success',
             'data' => new ShowCommodityResource($commodity),
+        ], Response::HTTP_OK);
+    }
+
+
+    /**
+     * Generate QR Code untuk barang tertentu dan kembalikan sebagai data SVG.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function generateQrCode($id)
+    {
+        $commodity = Commodity::find($id);
+        if (!$commodity) {
+            return response()->json(['success' => false, 'message' => 'Barang tidak ditemukan'], Response::HTTP_NOT_FOUND);
+        }
+
+        $verificationUrl = url('verify/qrcode/' . Crypt::encryptString($id));
+
+        $svgImage = QrCode::format('svg')->size(250)->margin(1)->generate($verificationUrl);
+        $qrCodeDataUri = 'data:image/svg+xml;base64,' . base64_encode($svgImage);
+        return response()->json([
+            'success'   => true,
+            'svg'       => $qrCodeDataUri,
+            'filename'  => 'qrcode-' . $commodity->name . '.svg'
         ], Response::HTTP_OK);
     }
 }
