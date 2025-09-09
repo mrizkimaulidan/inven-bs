f30b6d29c267b2b51accc257141b1cafbf6b7551
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2025-09-09 21:04:37 +0800

    ui: :lipstick: redesign ui for commodity acquisition crud modal

--- resources/views/commodity-acquisitions/modal/edit.blade.php
+++ resources/views/commodity-acquisitions/modal/edit.blade.php
@@ -1,40 +1,68 @@
-<!-- Modal -->
-<div class="modal fade" id="commodity_acquisition_edit_modal" data-backdrop="static" data-keyboard="false"
-	tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="commodity_acquisition_edit_modal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+	role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="staticBackdropLabel">Ubah Data Perolehan</h5>
+				<h5 class="modal-title" id="modalLabel">Ubah Data Perolehan</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-			<div class="modal-body">
-				<form method="POST">
-					@csrf
-					@method('PUT')
+			<form method="POST" id="editForm">
+				@csrf
+				@method('PUT')
+				<div class="modal-body">
+					<!-- Info Alert -->
+					<div class="alert alert-warning" role="alert">
+						<i class="fa-solid fa-circle-info mr-2"></i>
+						Kolom yang memiliki tanda merah <span class="font-weight-bold">wajib diisi.</span>
+					</div>
+
+					<!-- First Row: Basic Information -->
 					<div class="row">
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="name">Nama Perolehan</label>
-								<input type="text" name="name" class="form-control" id="name" placeholder="Masukan nama..">
+								<label for="name">
+									Nama Perolehan <span class="text-danger">*</span>
+								</label>
+								<input type="text" class="form-control @error('name', 'update') is-invalid @enderror" name="name"
+									id="name" value="{{ old('name') }}" placeholder="Masukan nama perolehan" required>
+								@error('name', 'update')
+								<div class="invalid-feedback d-block">
+									{{ $message }}
+								</div>
+								@enderror
 							</div>
 						</div>
 
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="description">Deskripsi Perolehan</label>
-								<textarea name="description" class="form-control" id="description" style="height: 100px;"
-									placeholder="Masukan deskripsi (opsional).."></textarea>
+								<label for="description">
+									Deskripsi Perolehan <small class="text-muted">(opsional)</small>
+								</label>
+								<textarea class="form-control @error('description', 'update') is-invalid @enderror" name="description"
+									id="description" rows="3"
+									placeholder="Masukan deskripsi perolehan">{{ old('description') }}</textarea>
+								@error('description', 'update')
+								<div class="invalid-feedback d-block">
+									{{ $message }}
+								</div>
+								@enderror
 							</div>
 						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-						<button type="submit" class="btn btn-success">Ubah</button>
-					</div>
-				</form>
-			</div>
+				</div>
+
+				<!-- Modal Footer -->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">
+						<i class="fas fa-times mr-1"></i> Tutup
+					</button>
+					<button type="submit" class="btn btn-success">
+						<i class="fas fa-floppy-disk mr-1"></i> Simpan Perubahan
+					</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
--- resources/views/commodity-acquisitions/modal/show.blade.php
+++ resources/views/commodity-acquisitions/modal/show.blade.php
@@ -1,34 +1,42 @@
-<!-- Modal -->
-<div class="modal fade" id="show_commodity_acquisition" data-backdrop="static" data-keyboard="false"
-	tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="show_commodity_acquisition" data-backdrop="static" data-keyboard="false" tabindex="-1"
+	role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="staticBackdropLabel">Detail Data Perolehan</h5>
+				<h5 class="modal-title" id="modalLabel">Detail Data Perolehan</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
+
 			<div class="modal-body">
+				<!-- First Row: Basic Information -->
 				<div class="row">
-					<div class="col-lg-12">
+					<div class="col-md-12">
 						<div class="form-group">
-							<label for="name">Nama Perolehan</label>
-							<input type="text" name="name" class="form-control" id="name" disabled>
+							<label for="name">
+								Nama Perolehan
+							</label>
+							<input type="text" class="form-control" id="name" disabled>
 						</div>
 					</div>
 
-					<div class="col-lg-12">
+					<div class="col-md-12">
 						<div class="form-group">
-							<label for="description">Deskripsi Perolehan</label>
-							<textarea name="description" class="form-control" id="description" style="height: 100px;"
-								disabled></textarea>
+							<label for="description">
+								Deskripsi Perolehan
+							</label>
+							<textarea class="form-control" id="description" disabled></textarea>
 						</div>
 					</div>
 				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-				</div>
+			</div>
+
+			<!-- Modal Footer -->
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">
+					<i class="fas fa-times mr-1"></i> Tutup
+				</button>
 			</div>
 		</div>
 	</div>
