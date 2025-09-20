0856f68a841b847aa1a0ed297519d38ae7a0999f
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2025-09-21 01:31:18 +0800

    ui: :lipstick: redesign ui modal for role and permission crud page

--- resources/views/roles/modal/create.blade.php
+++ resources/views/roles/modal/create.blade.php
@@ -1,42 +1,45 @@
-<!-- Modal -->
 <div class="modal fade" id="role_create_modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
 	aria-labelledby="staticBackdropLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="staticBackdropLabel">Tambah Data Peran dan Hak Akses</h5>
+				<h5 class="modal-title" id="modalLabel">Tambah Data Peran dan Hak Akses</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-			<div class="modal-body">
-				<div class="d-flex align-items-center">
-					<i class="text-warning fa-solid fa-circle-info mr-2"></i>
-					<p class="font-italic mb-0">
+			<form action="{{ route('peran-dan-hak-akses.store') }}" method="POST">
+				@csrf
+
+				<div class="modal-body">
+					<!-- Info Alert -->
+					<div class="alert alert-warning" role="alert">
+						<i class="fa-solid fa-circle-info mr-2"></i>
 						Kolom yang memiliki tanda merah <span class="font-weight-bold">wajib diisi.</span>
-					</p>
-				</div>
-				<hr>
-				<form action="{{ route('peran-dan-hak-akses.store') }}" method="POST">
-					@csrf
+					</div>
+
+					<!-- First Row: Basic Information -->
 					<div class="row">
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="name">Nama Peran<span class="font-weight-bold text-danger">*</span></label>
-								<input type="text" name="name" id="name"
-									class="form-control @error('name', 'store') is-invalid @enderror" value="{{ old('name') }}"
-									placeholder="Masukan nama peran..">
+								<label for="name">
+									Nama Peran <span class="text-danger">*</span>
+								</label>
+								<input type="text" class="form-control @error('name', 'store') is-invalid @enderror" name="name"
+									id="name" value="{{ old('name') }}" placeholder="Masukan nama peran" required>
 								@error('name', 'store')
-								<div class="d-block invalid-feedback">
+								<div class="invalid-feedback d-block">
 									{{ $message }}
 								</div>
 								@enderror
 							</div>
 						</div>
 
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="permissions">Daftar Hak Akses<span class="font-weight-bold text-danger">*</span></label>
+								<label for="permissions">
+									Daftar Hak Akses <span class="text-danger">*</span>
+								</label>
 								<select multiple class="tom-select @error('permissions', 'store') is-invalid @enderror"
 									name="permissions[]" id="permissions" placeholder="Pilih atau ketik hak akses..">
 									@foreach ($permissions as $permission)
@@ -44,19 +47,25 @@
 									@endforeach
 								</select>
 								@error('permissions', 'store')
-								<div class="d-block invalid-feedback">
+								<div class="invalid-feedback d-block">
 									{{ $message }}
 								</div>
 								@enderror
 							</div>
 						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-						<button type="submit" class="btn btn-success">Tambah</button>
-					</div>
-				</form>
-			</div>
+				</div>
+
+				<!-- Modal Footer -->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">
+						<i class="fas fa-times mr-1"></i> Tutup
+					</button>
+					<button type="submit" class="btn btn-success">
+						<i class="fas fa-plus mr-1"></i> Tambah
+					</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
--- resources/views/roles/modal/edit.blade.php
+++ resources/views/roles/modal/edit.blade.php
@@ -1,56 +1,72 @@
-<!-- Modal -->
 <div class="modal fade" id="role_edit_modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
 	aria-labelledby="staticBackdropLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="staticBackdropLabel">Ubah Data Peran dan Hak Akses</h5>
+				<h5 class="modal-title" id="modalLabel">Ubah Data Peran dan Hak Akses</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-			<div class="modal-body">
-				<form method="POST">
-					@csrf
-					@method('PUT')
+			<form method="POST">
+				@csrf
+				@method('PUT')
+
+				<div class="modal-body">
+					<!-- Info Alert -->
+					<div class="alert alert-warning" role="alert">
+						<i class="fa-solid fa-circle-info mr-2"></i>
+						Kolom yang memiliki tanda merah <span class="font-weight-bold">wajib diisi.</span>
+					</div>
+
+					<!-- First Row: Basic Information -->
 					<div class="row">
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="name">Nama Peran</label>
-								<input type="text" name="name" id="name"
-									class="form-control @error('name', 'store') is-invalid @enderror" value="{{ old('name') }}"
-									placeholder="Masukan nama peran..">
-								@error('name', 'store')
-								<div class="d-block invalid-feedback">
+								<label for="name">
+									Nama Peran <span class="text-danger">*</span>
+								</label>
+								<input type="text" class="form-control @error('name', 'update') is-invalid @enderror" name="name"
+									id="name" value="{{ old('name') }}" placeholder="Masukan nama peran" required>
+								@error('name', 'update')
+								<div class="invalid-feedback d-block">
 									{{ $message }}
 								</div>
 								@enderror
 							</div>
 						</div>
 
-						<div class="col-lg-12">
+						<div class="col-md-12">
 							<div class="form-group">
-								<label for="permissions">Daftar Hak Akses</label>
-								<select multiple class="tom-select @error('permissions', 'store') is-invalid @enderror"
+								<label for="permissions">
+									Daftar Hak Akses <span class="text-danger">*</span>
+								</label>
+								<select multiple class="tom-select @error('permissions', 'update') is-invalid @enderror"
 									name="permissions[]" id="permissions" placeholder="Pilih atau ketik hak akses..">
 									@foreach ($permissions as $permission)
 									<option value="{{ $permission->id }}">{{ $permission->name }}</option>
 									@endforeach
 								</select>
-								@error('permissions', 'store')
-								<div class="d-block invalid-feedback">
+								@error('permissions', 'update')
+								<div class="invalid-feedback d-block">
 									{{ $message }}
 								</div>
 								@enderror
 							</div>
 						</div>
 					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-						<button type="submit" class="btn btn-success">Ubah</button>
-					</div>
-				</form>
-			</div>
+				</div>
+
+				<!-- Modal Footer -->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">
+						<i class="fas fa-times mr-1"></i> Tutup
+					</button>
+					<button type="submit" class="btn btn-success">
+						<i class="fas fa-floppy-disk mr-1"></i> Simpan Perubahan
+					</button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
