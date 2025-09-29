2f1687145414efebef2208c9bc829b8f7fc5a0be
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2025-09-30 00:34:55 +0800

    chore: removed @error() blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,8 +43,8 @@
 								<div class="d-block">
 									<label for="password" class="control-label">Password</label>
 								</div>
-								<input id="password" type="password" class="form-control @error('password') @enderror" name="password"
-									tabindex="2" placeholder="Masukan kata sandi.." required>
+								<input id="password" type="password" class="form-control" name="password" tabindex="2"
+									placeholder="Masukan kata sandi.." required>
 								<div class="invalid-feedback">
 									Mohon masukkan password!
 								</div>
