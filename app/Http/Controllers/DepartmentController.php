dd5a1bd75be9de7ac81fa4822ed4ae60b2a6ab68
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-10-11 12:48:26 +0700

    feat(department): create DepartmentController with CRUD operations

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Department;
+use App\Http\Requests\StoreDepartmentRequest;
+use App\Http\Requests\UpdateDepartmentRequest;
+
+class DepartmentController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Department::class, 'commodity_acquisition');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $departments = Department::orderBy('name', 'ASC')->get();
+
+        return view('commodity-acquisitions.index', compact('departments'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDepartmentRequest $request)
+    {
+        Department::create($request->validated());
+
+        return to_route('perolehan.index')->with('success', 'Data berhasil ditambahkan!');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDepartmentRequest $request, Department $department)
+    {
+        $department->update($request->validated());
+
+        return to_route('perolehan.index')->with('success', 'Data berhasil diubah!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+
+        return to_route('perolehan.index')->with('success', 'Data berhasil dihapus!');
+    }
+}
