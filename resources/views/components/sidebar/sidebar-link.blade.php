934f924b7c6746121c07c145d56ae2f8c06b3bb9
mrizkimaulidan <mrizkimaulidan@icloud.com>
2026-08-24 12:54:34 +0800

    removed

--- resources/views/components/sidebar/sidebar-link.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-	<li class="nav-item dropdown{{ $active ? ' active' : '' }}">
-		<a href="{{ $link }}" class="nav-link"><i class="{{ $icon }}"></i><span>{{ $name }}</span></a>
-	</li>
-</div>
